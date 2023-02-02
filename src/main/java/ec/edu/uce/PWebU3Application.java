a54649e05ffd49b33dbe1cf479df6c82b71a3343
jessicaly <jessiclyaruiz@hotmail.com>
2023-02-01 23:49:14 -0500

    deber u3

---
+++ src/main/java/ec/edu/uce/PWebU3Application.java
@@ -0,0 +1,13 @@
+package ec.edu.uce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PWebU3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PWebU3Application.class, args);
+	}
+
+}
